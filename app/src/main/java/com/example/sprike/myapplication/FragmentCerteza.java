346d6b90b6f3caae2159938a709598b91abaa6e4
kurtteodoro22 <37031481+kurtteodoro22@users.noreply.github.com>
2018-03-03 23:57:20 -0300

    Update FragmentCerteza.java

--- app/src/main/java/com/example/sprike/myapplication/FragmentCerteza.java
+++ app/src/main/java/com/example/sprike/myapplication/FragmentCerteza.java
@@ -1,14 +1,22 @@
 package com.example.sprike.myapplication;
 
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class FragmentCerteza extends Dialog {
+public class FragmentCerteza extends DialogFragment {
 
-    public FragmentCerteza(@NonNull Context context) {
-        super(context);
-        setContentView(R.layout.fragmento_dialog);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmento_dialog, container, false);
+        return view;
     }
 
 
